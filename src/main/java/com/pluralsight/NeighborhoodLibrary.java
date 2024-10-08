ac3df361ead410942af49e18814b0bc9f9d770d5
KoshkinaKate <koshkinaekaterina4987@gmail.com>
2024-10-08 17:26:01 -0400

    added array of 5 books

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -3,5 +3,14 @@ package com.pluralsight;
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
 
+        Book [] inventory = new Book[5]; //declaring an array (object of type Book)
+
+        inventory[0] = new Book (1, "789-092-45-76","Pride and Prejudice");
+        inventory[1] = new Book (1, "789-4r4-f3-00","Anna Karenina");
+        inventory[2] = new Book (1, "789-oo9-10-09","To Kill a Mockingbird");
+        inventory[3] = new Book (1, "789-i91-13-12","War and Peace");
+        inventory[4] = new Book (1, "789-06u-09-89","Republic");
+
+
     }
 }
